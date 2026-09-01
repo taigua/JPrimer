c7a7c41cacafaab26db69b35d55d0ae90c405bd1
taigua <corwin.dong@hotmail.com>
2026-09-01 10:59:52 +0800

    Translate to 017

--- book/html/searcher-09f2665d.js
+++ book/html/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2986e3d7.js',
+                path_to_root + 'searchindex-14b4b251.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
